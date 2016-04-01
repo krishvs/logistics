e3b35a62e50ca5edbc9dd44e01489151037830d7
Prelang Builder <builder@prelang.com>
2016-04-01 14:27:26 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Account' to allow for create, update

--- app/admin/account.rb
+++ app/admin/account.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Account do
+  permit_params :company
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
